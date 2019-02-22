8bd4eea82ff19a413c1593cc6c6a77824a44130e
xeniasuper <ichmalegern@gmail.com>
2019-02-23 01:26:50 +0300

    Add a solution to the Duplicate Count problem

--- codewars.js
+++ codewars.js
@@ -129,3 +129,43 @@ String.prototype.toJadenCase = function () {
 };
 
 //console.log("make me jaden case".toJadenCase());
+
+// Problem 6
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric
+// digits that occur more than once in the input string. The input string can be assumed to contain only alphabets
+// (both uppercase and lowercase) and numeric digits.
+//
+//  Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+/**
+ * Counts distinct case-insensitive alphabetic characters and numeric digits
+ * that occur more than once in the input string
+ * @param {string} text
+ * @return {number}
+ */
+function duplicateCount(text){
+    let occurrences = {};
+    let textLower = text.toLowerCase();
+
+    for (let i = 0; i < textLower.length; i++) {
+        if (!occurrences.hasOwnProperty(textLower[i])) {
+            occurrences[textLower[i]] = 1;
+        } else {
+            occurrences[textLower[i]] += 1;
+        }
+    }
+
+    let counter = 0;
+    for (let letter in occurrences) {
+        if (occurrences[letter] > 1) counter++;
+    }
+
+    return counter;
+}
